f464b4afae128acceb106c56354689d6f6729a8a
Maël Nison <nison.mael@gmail.com>
2012-09-11 13:28:08 -0400

    Adds variable initialization during declaration

--- sources/builder/CodeGenerator/stmt/decl/Variables.cc
+++ sources/builder/CodeGenerator/stmt/decl/Variables.cc
@@ -6,8 +6,14 @@ using builder::CodeGenerator;
 
 void CodeGenerator::visit( ast::stmt::decl::Variables & astVariablesDeclarationStatement )
 {
-    for ( auto & variable : astVariablesDeclarationStatement.variables( ) )
-        mClosureStack.top( )->declare( variable.name( ) );
+    for ( auto & variable : astVariablesDeclarationStatement.variables( ) ) {
+        if ( variable.initializer( ) ) {
+            variable.initializer( )->accept( * this );
+            mClosureStack.top( )->declare( variable.name( ), mValue.release( ) );
+        } else {
+            mClosureStack.top( )->declare( variable.name( ) );
+        }
+    }
 
     if ( astVariablesDeclarationStatement.next( ) ) {
         astVariablesDeclarationStatement.next( )->accept( * this );
