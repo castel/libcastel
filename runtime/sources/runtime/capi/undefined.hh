ed26638ea1c31d3a32431a07a0542c567794fa16
Maël Nison <nison.mael@gmail.com>
2013-05-19 15:18:02 +0200

    Removes extraneous header

--- runtime/sources/runtime/capi/undefined.hh
+++
@@ -1,8 +0,0 @@
-#include "castel/runtime/boxes/Undefined.hh"
-#include "castel/runtime/capi/Undefined.hh"
-#include "castel/runtime/helper/create.hh"
-
-castel::runtime::boxes::Undefined * castelUndefined_create( void )
-{
-    return castel::runtime::helper::create< castel::runtime::boxes::Undefined >( );
-}
